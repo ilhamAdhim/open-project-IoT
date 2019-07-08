81376e56a9b6edb2c016ddf157d6953943b31be1
ilhamAdhim <ilhamm179@gmail.com>
2019-07-08 15:15:14 +0700

    bug multi timer lamp fixed

--- script.js
+++ script.js
@@ -13,14 +13,15 @@ firebase.initializeApp(firebaseConfig);
 
 // Get the Database service for the default app
 let database = firebase.database();
-
-var timer
-var totalSeconds
+var lampsCollectionTimer = [null, ]
+var totalSec, timer, isTurnOn
+var trial = 0
 
 let status1 = document.getElementById("status1");
 let status2 = document.getElementById("status2");
 let status3 = document.getElementById("status3");
 
+let allButton = document.getElementById("allButton");
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
@@ -43,21 +44,24 @@ function checkLampStatus() {
 }
 
 function turnOn(led) {
+    allButton.innerHTML = 'Turn On All'
+    allButton.style.backgroundColor = 'rgb(250, 184, 2)'
+    allButton.style.color = 'rgb(0, 0, 0)'
     buttonLamps[led].style.backgroundColor = 'rgb(250, 184, 2)'
     buttonLamps[led].setAttribute("class", "btn btn-warning")
     buttonLamps[led].innerHTML = 'Turn On'
     buttonLamps[led].style.color = 'rgb(0, 0, 0)'
 
-
 }
 
 function turnOff(led) {
+    allButton.innerHTML = 'Turn Off All'
+    allButton.style.backgroundColor = 'rgba(54, 58, 50, 0.596)'
+    allButton.style.color = 'rgb(199, 184, 184)'
     buttonLamps[led].setAttribute("class", "btn")
     buttonLamps[led].style.backgroundColor = 'rgba(54, 58, 50, 0.596)'
     buttonLamps[led].innerHTML = 'Turn Off'
     buttonLamps[led].style.color = 'rgb(199, 184, 184)'
-
-    timerLamp(led)
 
 }
 
@@ -68,15 +72,17 @@ function readLampStatus(led) {
             statusLampsHTML[led].innerHTML = 'On';
             statusLampsHTML[led].style.color = 'black';
             statusLampsHTML[led].style.backgroundColor = 'rgb(236, 193, 1)';
+            isTurnOn = true
             turnOff(led);
-
         } else {
+
             statusLampsHTML[led].innerHTML = 'Off';
             statusLampsHTML[led].style.color = 'rgb(230, 222, 222)';
             statusLampsHTML[led].style.backgroundColor = "";
+            isTurnOn = false
             turnOn(led);
-            // pauseTimerLamp(led)
         }
+        timerLamp(led, isTurnOn)
     })
 }
 
@@ -85,53 +91,62 @@ function onclickLamps(led) {
     buttonLamps[led].addEventListener('click', setTimeout(writeToFirebase(led), 1000))
 }
 
+function onclickAllLamps() {
+    allButton.addEventListener('click', setTimeout(writeAllToFirebase(), 1000))
+}
+
 function writeToFirebase(led) {
     let updates = {}
     updates[`leds/` + led] = ((buttonLamps[led].innerText) === 'Turn Off') ? 0 : 1
     database.ref().update(updates)
 }
 
+function writeAllToFirebase(led) {
+    let updateAllLamps = {}
+    for (let index = 1; index <= 3; index++)
+        updateAllLamps[`leds/` + index] = (allButton.innerText == 'Turn Off All') ? 0 : 1
+    database.ref().update(updateAllLamps)
+}
 
+//TIMER SECTION 
+//--> count up 3 lampu menumpuk perhitungan totalSec ( Jul 6 ) ===> Solusi : pakai OOP dan masukkan ke array
+//--> ada bug timer tetep lanjut walau di turn off lampnya (Jul 8 : 2.40 PM) ===> Pakai OOP dan update array di index tertentu
+//--> SUDAH SELESAI DONG HAHAHAHA ( Jul 8 : 3.10PM) 
+// Note 8 Juli 2019 : Client harus menyalakan seluruh lampu dulu, supaya disinkronisasi dengan kodingan ini
 
-//TIMER SECTION
+function lampHandler(led, totalSec, timer) {
+    this.led = led
+    this.totalSec = totalSec
+    this.timer = timer
+}
 
-
-let countLamps = [null,
-    {
-        totalSeconds: 0,
-    },
-
-    {
-        totalSeconds: 0,
-    },
-
-    {
-        totalSeconds: 0,
+function timerLamp(led, isTurnOn) {
+    if (timerLamps[led].innerHTML && isTurnOn) {
+        console.log("Timer of lamp " + led + " is already exist")
+        resumeTimer()
+    } else if (isTurnOn == false) {
+        stopTimer()
+    } else {
+        lampsCollectionTimer.push(new lampHandler(led, parseInt(0), setInterval(start, 1000)))
     }
-]
-
-function timerLamp(led) {
-    timer = setInterval(start, 1000)
-    totalSeconds = 0
 
     function start() {
-        totalSeconds++
-        hour = Math.floor(totalSeconds / 3600)
-        minute = Math.floor((totalSeconds - hour * 3600) / 60)
-        seconds = totalSeconds - (hour * 3600 + minute * 60)
+        lampsCollectionTimer[led].totalSec++
+        hour = Math.floor(lampsCollectionTimer[led].totalSec / 3600)
+        minute = Math.floor((lampsCollectionTimer[led].totalSec - hour * 3600) / 60)
+        seconds = lampsCollectionTimer[led].totalSec - (hour * 3600 + minute * 60)
         timerLamps[led].innerHTML = hour + " : " + minute + " : " + seconds
     }
 
     function stopTimer() {
-        timer = clearInterval(timer)
+        console.log("timer at lamp " + led + " stopped")
+        let timer = lampsCollectionTimer[led].timer
+        clearInterval(timer)
     }
-}
 
-function startTimer(led) {
-    timer = setInterval(start, 1000)
-    totalSeconds = 0
-}
-
-function resumeTimer() {
-    timer = setInterval(start, 1000);
+    function resumeTimer() {
+        let tmpSecond = lampsCollectionTimer[led].totalSec
+        //create new object that starting totalSec is the value from previous totalSec before getting sremoved
+        lampsCollectionTimer[led] = new lampHandler(led, parseInt(tmpSecond), setInterval(start, 1000))
+    }
 }
